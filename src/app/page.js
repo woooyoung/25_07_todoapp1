55314bbef0641bb9cfff80160e3450f2ed1fb447
woooyoung <axdsw121@gmail.com>
2025-07-24 12:54:16 +0900

    테마, 아이콘, sub

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,46 @@
 'use client';
 
+import * as React from 'react';
 import Button from '@mui/material/Button';
+import { ThemeProvider } from '@mui/material';
+import { MdDeleteForever } from 'react-icons/md';
+import theme from './theme';
 
 export default function Home() {
   return (
     <>
-      <button>버튼</button>
-      <Button className="tw-bg-red-500" variant="contained">
-        Contained
-      </Button>
+      <ThemeProvider theme={theme}>
+        <div className="flex items-center gap-x-3">
+          <Button variant="text">Text</Button>
+          <Button
+            variant="contained"
+            onClick={() => {
+              confirm('삭제할거임?');
+            }}>
+            <MdDeleteForever />
+            삭제
+          </Button>
+          <Button variant="outlined">
+            <MdDeleteForever />
+            Outlined
+          </Button>
+        </div>
+      </ThemeProvider>
+      <div className="flex items-center gap-x-3">
+        <Button
+          variant="text"
+          onClick={() => {
+            alert('버튼 클릭 됨');
+          }}>
+          Text
+        </Button>
+        <Button variant="contained" disabled>
+          Contained
+        </Button>
+        <Button variant="outlined" href="sub/">
+          sub로 이동
+        </Button>
+      </div>
     </>
   );
 }
