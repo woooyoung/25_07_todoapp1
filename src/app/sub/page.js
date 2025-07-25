153226720ac2d5d1a74531eb9e5c7817f1d48b97
woooyoung <axdsw121@gmail.com>
2025-07-25 12:49:08 +0900

    TodoApp 전체 흐름 구현 완료 - 추가/수정/삭제 기능 + 드로어/모달/스낵바 UI 구성

--- src/app/sub/page.js
+++ src/app/sub/page.js
@@ -1,12 +1,7 @@
 'use client';
 
 import * as React from 'react';
-import Button from '@mui/material/Button';
 
 export default function Home() {
-  return (
-    <>
-      <h1>sub 페이지</h1>
-    </>
-  );
+  return <h1>서브페이지</h1>;
 }
